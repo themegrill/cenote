523c81993cc340107e5dcc55032a703f09d6a570
Ashesh Thapa <asheshmagar8888667@gmail.com>
2020-05-21 16:11:14 +0545

    Include - class-cenote-welcome-notice.php to functions.php

--- functions.php
+++ functions.php
@@ -343,6 +343,7 @@ if ( is_admin() ) {
     require get_template_directory() . '/inc/admin/class-cenote-notice.php';
 	require get_template_directory() . '/inc/admin/class-cenote-theme-review-notice.php';
     require get_template_directory() . '/inc/admin/class-cenote-upgrade-notice.php';
+    require get_template_directory() . '/inc/admin/class-cenote-welcome-notice.php';
 	require get_template_directory() . '/inc/admin/class-cenote-tdi-notice.php';
 }
 
