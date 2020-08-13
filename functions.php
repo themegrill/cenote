1691eb8a99c8a6c7754143023ad71c3ca962725a
ankitkarki17 <ankitkarki17@gmail.com>
2020-08-13 14:05:47 +0545

    Remove - TDI notice

--- functions.php
+++ functions.php
@@ -389,5 +389,4 @@ if ( is_admin() ) {
 	require get_template_directory() . '/inc/admin/class-cenote-upgrade-notice.php';
 	require get_template_directory() . '/inc/admin/class-cenote-dashboard.php';
 	require get_template_directory() . '/inc/admin/class-cenote-welcome-notice.php';
-	require get_template_directory() . '/inc/admin/class-cenote-tdi-notice.php';
 }
